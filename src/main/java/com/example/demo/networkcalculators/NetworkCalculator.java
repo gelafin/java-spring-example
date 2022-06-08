0b80283e0f3864bc6e123e4489468646324326e1
gelafin <themarkymarkers@gmail.com>
2022-06-08 12:32:59 -0700

    translate calculator implementation from Python repo

--- src/main/java/com/example/demo/networkcalculators/NetworkCalculator.java
+++ src/main/java/com/example/demo/networkcalculators/NetworkCalculator.java
@@ -1,10 +1,13 @@
 package com.example.demo.networkcalculators;
 
+import java.util.ArrayList;
+
 /**
- * Provides fields for simulating the Tcp slowstart phase
+ * Provides fields for simulating a packet group in the Tcp slowstart phase.
+ * Intended to occur within arrays.
  */
-record TcpSlowstartSimulation(
-    Integer groupNumber, Integer congestionWindowBytes, Integer congestionWindowMss, Integer[] packetsSentThisGroup
+record TcpSlowstartSimulationGroup(
+    Integer groupNumber, Integer congestionWindowBytes, Integer congestionWindowMss, ArrayList<Integer> packetsSentThisGroup
 ) {}
 
 /**
@@ -18,12 +21,63 @@ public class NetworkCalculator {
     /**
      * Simulates the TCP slow-start phase by calculating changes in congestion window size
      * @param mssBytes Maximum Segment Size, in bytes
-     * @return simulation result record
+     * @param slowstartCongestionWindowLimitBytes congestion window size limit for the slow-start phase, in bytes
+     * @param packetCount how many packets are waiting to be sent
+     * @param packetSizeBytes Size of each packet. If 0, packet_size will be set to mss_bytes
+     * @return simulation result record showing data for each group
      */
-    public static TcpSlowstartSimulation simulateTcpSlowstart(
+    public static ArrayList<TcpSlowstartSimulationGroup> simulateTcpSlowstart(
         Integer mssBytes, Integer slowstartCongestionWindowLimitBytes, Integer packetCount, Integer packetSizeBytes
     ) {
-        TcpSlowstartSimulation resultOut = new TcpSlowstartSimulation(1, 1, 1, new Integer[]{1});
+        ArrayList<TcpSlowstartSimulationGroup> resultOut = new ArrayList<TcpSlowstartSimulationGroup>();
+        packetSizeBytes = packetSizeBytes == 0 ? mssBytes : packetSizeBytes;
+        int currentCongestionWindowMss = 1;
+        int currentCongestionWindowBytes = mssBytes * currentCongestionWindowMss;
+        int packetIndex = 0;
+        int groupNumber = 1;
+
+        while (packetIndex < packetCount) {
+            // form a packet group according to congestion window size
+            int packetGroupBytes = 0;
+            ArrayList<Integer> packetGroupNumbers = new ArrayList<Integer>();
+            int packetNumberOut = packetIndex + 1; // start numbering from 1
+
+            // while there's still room (and waiting packets)
+            while (
+                packetGroupBytes <= currentCongestionWindowBytes - packetSizeBytes &&
+                packetNumberOut - 1 < packetCount
+            ) {
+                // add a packet
+                packetGroupNumbers.add(packetNumberOut);
+
+                // maintain loop
+                packetGroupBytes += packetSizeBytes;
+                ++packetNumberOut;
+            }
+
+            // save this packet group's details for return
+            TcpSlowstartSimulationGroup simulationGroupOut = new TcpSlowstartSimulationGroup(
+                groupNumber, currentCongestionWindowBytes, currentCongestionWindowMss, packetGroupNumbers
+            );
+            resultOut.add(simulationGroupOut);
+
+            // increase congestion window size so the next packet group can be bigger
+            if (currentCongestionWindowBytes >= slowstartCongestionWindowLimitBytes) {
+                // at the limit; increase linearly
+                ++currentCongestionWindowMss;
+            } else {
+                // not at the limit; increase exponentially
+                currentCongestionWindowMss *= 2;
+            }
+
+            // update current_congestion_window_mss's dependent variable
+            currentCongestionWindowBytes = mssBytes * currentCongestionWindowMss;
+
+            // maintain loop
+            ++groupNumber;
+            packetIndex += packetGroupNumbers.size();
+        }
+
         return resultOut;
     }
 }
--- src/main/java/com/example/demo/networkcalculators/NetworkCalculatorController.java
+++ src/main/java/com/example/demo/networkcalculators/NetworkCalculatorController.java
@@ -1,5 +1,7 @@
 package com.example.demo.networkcalculators;
 
+import java.util.ArrayList;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,14 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class NetworkCalculatorController {
 
 	@GetMapping("/simulate-tcp-slowstart")
-	public TcpSlowstartSimulation simulateTcpSlowstart(
+	public ArrayList<TcpSlowstartSimulationGroup> simulateTcpSlowstart(
 		@RequestParam(defaultValue = "1500") Integer mssBytes,
 		@RequestParam(defaultValue = "10500") Integer slowstartCongestionWindowLimitBytes,
 		@RequestParam(defaultValue = "15") Integer packetCount,
 		@RequestParam(defaultValue = "0") Integer packetSizeBytes
 	) {
 		return NetworkCalculator.simulateTcpSlowstart(
-			mssBytes, 11680, 15, 0
+			mssBytes, slowstartCongestionWindowLimitBytes, packetCount, packetSizeBytes
 		);
 	}
 }
